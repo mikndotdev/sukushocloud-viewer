f81e8f427ef52b0da3074fcb6d4173190d3bb0fa
maamokun/MikanDev <maamo-kun@outlook.com>
2024-10-11 00:27:29 +0900

    init: routing and Cloudflare ready setup

---
+++ app/[slug]/page.tsx
@@ -0,0 +1,11 @@
+export const runtime = "experimental-edge";
+
+export default function Viewer () {
+    return (
+        <>
+            <div className="relative">
+                Coming soon...
+            </div>
+        </>
+    );
+}
