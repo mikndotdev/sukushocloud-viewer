0198c13ba7a345e00c9963ba1ce48c2cbf541278
maamokun/MikanDev <maamo-kun@outlook.com>
2024-10-14 23:44:51 +0900

    add: branding and abuse report link

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,6 +1,13 @@
 // app/image/[id]/page.tsx
 export const runtime = "experimental-edge";
+import Image from "next/image";
+import Link from "next/link";
 import { getMetadata, ImageComponent } from "@/app/components/ImageComponent";
+
+import { Card, CardTitle } from "../components/shadcn/Card";
+import { FaRegStar } from "react-icons/fa";
+import { Button } from "../components/shadcn/Button";
+import KawaiiLogo from "../assets/mikan-vtube.svg";
 
 interface PageProps {
     params: { slug: string };
@@ -34,8 +41,33 @@ export async function generateMetadata({ params }: PageProps) {
 
 export default function Page({ params }: PageProps) {
     return (
+        <main>
         <div className="relative">
             <ImageComponent id={params.slug} />
         </div>
+        <div className="flex flex-col items-center justify-center w-full">
+                <Card className="mt-10 mb-5 w-10/12 bg-[#2f1c42] border-[#2f1c42] h-20 flex items-center justify-between px-4">
+                    <div className="flex items-center space-x-4">
+                        <div className="flex flex-col justify-center mr-5">
+                        <CardTitle className="text-xl text-white">
+                            Screenshot hosted by sukushocloud
+                        </CardTitle>
+                        <CardTitle className="text-md text-[#ff9900]">
+                            a MikanDev service
+                        </CardTitle>
+                        </div>
+                        <Link href="https://sukusho.cloud/">
+                        <Button className="text-white">
+                            <FaRegStar className="text-xl mr-3" />
+                            Try it free!
+                        </Button>
+                        </Link>
+                    </div>
+                    <Link href="https://mikn.dev/">
+                    <Image src={KawaiiLogo.src} alt="Logo" width={100} height={100} />
+                    </Link>
+                </Card>
+            </div>
+        </main>
     );
 }
--- app/components/interactiveFooter.tsx
+++ app/components/interactiveFooter.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import { FaDownload, FaLink } from "react-icons/fa";
+import { PiWarningFill } from "react-icons/pi";
 import { Button } from "./shadcn/Button";
 import { toast } from "sonner";
+import Link from "next/link";
 
 interface InteractiveCardFooterProps {
     imageUrl: string;
@@ -22,16 +24,22 @@ const InteractiveCardFooter: React.FC<InteractiveCardFooterProps> = ({
 
     return (
         <div className="flex items-center space-x-3">
-            <a href={imageUrl} download={imageName}>
+            <Link href={imageUrl} download={imageName}>
                 <Button>
                     <FaDownload className="text-xl mr-3" />
                     Download
                 </Button>
-            </a>
+            </Link>
             <Button onClick={() => copyURL(shortUrl)}>
                 <FaLink className="text-xl mr-3" />
                 Copy short URL
             </Button>
+            <Link href={`mailto:abuse@mikn.dev?subject=sukushocloud%20Abuse%20Report&body=Hello%3B%0A%0AI%20believe%20that%20${imageUrl}%20hosted%20on%20the%20sukushocloud%20platform%20may%20be%20against%20the%20Terms.`}>
+            <Button className="text-red-500">
+                <PiWarningFill className="text-xl mr-3" />
+                Report abuse
+            </Button>
+            </Link>
         </div>
     );
 };
